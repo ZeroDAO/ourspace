e3f51dd4ec06a6f8f0f884c57beb7e4fae1264f0
DarkingLee <xiuerdwy@gmail.com>
2021-08-14 18:49:12 +0800

    fix token trait bug

--- traits/src/token.rs
+++ traits/src/token.rs
@@ -4,5 +4,5 @@ pub trait MultiBaseToken<AccountId, Balance> {
 	fn get_bonus_amount() -> Balance;
 	fn staking(who: &AccountId, amount: &Balance) -> DispatchResult;
 	fn release(who: &AccountId, amount: &Balance) -> DispatchResult;
-	fn share(who: &AccountId, target: &AccountId) -> Result<Balance, DispatchError>;
+	fn share(who: &AccountId, target: &Vec<AccountId>) -> Result<Balance, DispatchError>;
 }
