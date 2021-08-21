138e6b19478b700b88808a2cf1d6690999c5ccd7
DarKing Lee <xiuerdwy@gmail.com>
2021-08-21 12:36:04 +0800

    test: trust pallet

--- pallets/trust/src/mock.rs
+++ pallets/trust/src/mock.rs
@@ -6,14 +6,21 @@ use frame_support::sp_runtime::{
     traits::{BlakeTwo256, IdentityLookup},
 };
 use frame_support::{construct_runtime, parameter_types, traits::GenesisBuild};
+
 pub use frame_system as system;
 use sp_core::H256;
 pub use sp_runtime::{Perbill, Permill};
+pub use zd_reputation;
 
 pub type AccountId = u64;
 pub type BlockNumber = u64;
 
 pub const ALICE: AccountId = 1;
+pub const BOB: AccountId = 2;
+pub const CHARLIE: AccountId = 3;
+pub const DAVE: AccountId = 4;
+pub const EVE: AccountId = 5;
+pub const FERDIE: AccountId = 6;
 
 pub const INIT_PERIOD: BlockNumber = 10;
 
@@ -76,7 +83,7 @@ impl system::Config for Test {
     type BlockNumber = u64;
     type Hash = H256;
     type Hashing = BlakeTwo256;
-    type AccountId = u64;
+    type AccountId = AccountId;
     type Lookup = IdentityLookup<Self::AccountId>;
     type Header = Header;
     type Event = Event;
--- pallets/trust/src/tests.rs
+++ pallets/trust/src/tests.rs
@@ -3,13 +3,77 @@
 use super::*;
 use crate::mock::*;
 use frame_support::{assert_noop, assert_ok, dispatch};
+use system::Account;
 
 fn initialize_trust() {
-    assert_eq!(ZdTrust::do_trust(&1,&2), Ok(()));
+    let relationships: Vec<(AccountId, AccountId)> =
+        vec![(FERDIE, BOB), (ALICE, CHARLIE), (ALICE, BOB), (BOB, CHARLIE), (CHARLIE, DAVE), (DAVE, EVE)];
+
+    for (a, b) in relationships.iter() {
+        assert_eq!(ZdTrust::do_trust(a, b), Ok(()));
+    }
 }
 
 fn new_test_ext() -> sp_io::TestExternalities {
     let mut ext = ExtBuilder::default().build();
     ext.execute_with(|| System::set_block_number(1));
     ext
+}
+
+#[test]
+fn get_trust_count_should_work() {
+    new_test_ext().execute_with(|| {
+        initialize_trust();
+        assert_eq!(ZdTrust::get_trust_count(&BOB), 1);
+        assert_eq!(ZdTrust::get_trust_count(&ALICE), 2);
+        assert_eq!(ZdTrust::get_trust_count(&BOB), 1);
+        assert_eq!(ZdTrust::get_trust_count(&CHARLIE), 1);
+    });
+}
+
+#[test]
+fn is_trust_should_work() {
+    new_test_ext().execute_with(|| {
+        initialize_trust();
+        assert_eq!(ZdTrust::is_trust(&FERDIE, &BOB), true);
+        assert_eq!(ZdTrust::is_trust(&ALICE, &BOB), true);
+        assert_eq!(ZdTrust::is_trust(&ALICE,&EVE), false);
+        assert_eq!(ZdTrust::is_trust(&20,&ALICE), false);
+        assert_eq!(ZdTrust::is_trust(&20,&21), false);
+        assert_eq!(ZdTrust::is_trust(&ALICE,&21), false);
+    });
+}
+
+#[test]
+fn valid_nodes_should_work() {
+    new_test_ext().execute_with(|| {
+        initialize_trust();
+        assert_ok!(ZdTrust::valid_nodes(&vec![ALICE,BOB]));
+        assert_ok!(ZdTrust::valid_nodes(&vec![ALICE,BOB,CHARLIE]));
+        assert_ok!(ZdTrust::valid_nodes(&vec![ALICE,BOB,CHARLIE,DAVE,EVE]));
+    });
+}
+
+#[test]
+fn valid_nodes_should_fail() {
+    new_test_ext().execute_with(|| {
+        initialize_trust();
+        assert_noop!(ZdTrust::valid_nodes(&vec![ALICE,BOB,DAVE]), Error::<Test>::WrongPath);
+        assert_noop!(ZdTrust::valid_nodes(&vec![ALICE,BOB,CHARLIE, 21]), Error::<Test>::WrongPath);
+        assert_noop!(ZdTrust::valid_nodes(&vec![21,BOB,CHARLIE]), Error::<Test>::WrongPath);
+        assert_noop!(ZdTrust::valid_nodes(&vec![21,22,23]), Error::<Test>::WrongPath);
+        assert_noop!(ZdTrust::valid_nodes(&vec![21,21,21]), Error::<Test>::WrongPath);
+        assert_noop!(ZdTrust::valid_nodes(&vec![ALICE,ALICE]), Error::<Test>::WrongPath);
+    });
+}
+
+#[test]
+fn computed_path_should_work() {
+    new_test_ext().execute_with(|| {
+        initialize_trust();
+        assert_ok!(ZdSeeds::new_seed(Origin::root(), ALICE));
+        // vec![(FERDIE, BOB), (ALICE, CHARLIE), (ALICE, BOB), (BOB, CHARLIE), (CHARLIE, DAVE), (DAVE, EVE)];
+        assert_ok!(ZdTrust::computed_path(&vec![ALICE,BOB]), (2,250));
+        assert_ok!(ZdTrust::computed_path(&vec![ALICE,BOB,DAVE]), (3,50));
+    });
 }
