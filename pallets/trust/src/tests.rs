c1da1162d465941ef67d6a7c4ab497be5e40177b
DarKing Lee <xiuerdwy@gmail.com>
2021-08-21 19:20:00 +0800

    test: trust pallet

--- pallets/trust/src/tests.rs
+++ pallets/trust/src/tests.rs
@@ -1,13 +1,18 @@
 #![cfg(test)]
 
 use super::*;
-use crate::mock::*;
-use frame_support::{assert_noop, assert_ok, dispatch};
-use system::Account;
+use crate::mock::{Event, *};
+use frame_support::{assert_noop, assert_ok};
 
 fn initialize_trust() {
-    let relationships: Vec<(AccountId, AccountId)> =
-        vec![(FERDIE, BOB), (ALICE, CHARLIE), (ALICE, BOB), (BOB, CHARLIE), (CHARLIE, DAVE), (DAVE, EVE)];
+    let relationships: Vec<(AccountId, AccountId)> = vec![
+        (FERDIE, BOB),
+        (ALICE, CHARLIE),
+        (ALICE, BOB),
+        (BOB, CHARLIE),
+        (CHARLIE, DAVE),
+        (DAVE, EVE),
+    ];
 
     for (a, b) in relationships.iter() {
         assert_eq!(ZdTrust::do_trust(a, b), Ok(()));
@@ -37,10 +42,10 @@ fn is_trust_should_work() {
         initialize_trust();
         assert_eq!(ZdTrust::is_trust(&FERDIE, &BOB), true);
         assert_eq!(ZdTrust::is_trust(&ALICE, &BOB), true);
-        assert_eq!(ZdTrust::is_trust(&ALICE,&EVE), false);
-        assert_eq!(ZdTrust::is_trust(&20,&ALICE), false);
-        assert_eq!(ZdTrust::is_trust(&20,&21), false);
-        assert_eq!(ZdTrust::is_trust(&ALICE,&21), false);
+        assert_eq!(ZdTrust::is_trust(&ALICE, &EVE), false);
+        assert_eq!(ZdTrust::is_trust(&20, &ALICE), false);
+        assert_eq!(ZdTrust::is_trust(&20, &21), false);
+        assert_eq!(ZdTrust::is_trust(&ALICE, &21), false);
     });
 }
 
@@ -48,9 +53,9 @@ fn is_trust_should_work() {
 fn valid_nodes_should_work() {
     new_test_ext().execute_with(|| {
         initialize_trust();
-        assert_ok!(ZdTrust::valid_nodes(&vec![ALICE,BOB]));
-        assert_ok!(ZdTrust::valid_nodes(&vec![ALICE,BOB,CHARLIE]));
-        assert_ok!(ZdTrust::valid_nodes(&vec![ALICE,BOB,CHARLIE,DAVE,EVE]));
+        assert_ok!(ZdTrust::valid_nodes(&vec![ALICE, BOB]));
+        assert_ok!(ZdTrust::valid_nodes(&vec![ALICE, BOB, CHARLIE]));
+        assert_ok!(ZdTrust::valid_nodes(&vec![ALICE, BOB, CHARLIE, DAVE, EVE]));
     });
 }
 
@@ -58,12 +63,30 @@ fn valid_nodes_should_work() {
 fn valid_nodes_should_fail() {
     new_test_ext().execute_with(|| {
         initialize_trust();
-        assert_noop!(ZdTrust::valid_nodes(&vec![ALICE,BOB,DAVE]), Error::<Test>::WrongPath);
-        assert_noop!(ZdTrust::valid_nodes(&vec![ALICE,BOB,CHARLIE, 21]), Error::<Test>::WrongPath);
-        assert_noop!(ZdTrust::valid_nodes(&vec![21,BOB,CHARLIE]), Error::<Test>::WrongPath);
-        assert_noop!(ZdTrust::valid_nodes(&vec![21,22,23]), Error::<Test>::WrongPath);
-        assert_noop!(ZdTrust::valid_nodes(&vec![21,21,21]), Error::<Test>::WrongPath);
-        assert_noop!(ZdTrust::valid_nodes(&vec![ALICE,ALICE]), Error::<Test>::WrongPath);
+        assert_noop!(
+            ZdTrust::valid_nodes(&vec![ALICE, BOB, DAVE]),
+            Error::<Test>::WrongPath
+        );
+        assert_noop!(
+            ZdTrust::valid_nodes(&vec![ALICE, BOB, CHARLIE, 21]),
+            Error::<Test>::WrongPath
+        );
+        assert_noop!(
+            ZdTrust::valid_nodes(&vec![21, BOB, CHARLIE]),
+            Error::<Test>::WrongPath
+        );
+        assert_noop!(
+            ZdTrust::valid_nodes(&vec![21, 22, 23]),
+            Error::<Test>::WrongPath
+        );
+        assert_noop!(
+            ZdTrust::valid_nodes(&vec![21, 21, 21]),
+            Error::<Test>::WrongPath
+        );
+        assert_noop!(
+            ZdTrust::valid_nodes(&vec![ALICE, ALICE]),
+            Error::<Test>::WrongPath
+        );
     });
 }
 
@@ -74,9 +97,12 @@ fn computed_path_should_work() {
         assert_ok!(ZdSeeds::new_seed(Origin::root(), ALICE));
         // vec![(FERDIE, BOB), (ALICE, CHARLIE), (ALICE, BOB), (BOB, CHARLIE), (CHARLIE, DAVE), (DAVE, EVE)];
         // 1000 / 2.max(5) / (1000 - 0).ln() = 28.5714
-        assert_ok!(ZdTrust::computed_path(&vec![ALICE,BOB]), (7,28));
+        assert_ok!(ZdTrust::computed_path(&vec![ALICE, BOB]), (7, 28));
         // 28 / 0.max(5) / 1 = 5.6
-        assert_ok!(ZdTrust::computed_path(&vec![ALICE,BOB,CHARLIE]), (1 + 7,5));
+        assert_ok!(
+            ZdTrust::computed_path(&vec![ALICE, BOB, CHARLIE]),
+            (1 + 7, 5)
+        );
     });
 }
 
@@ -85,8 +111,14 @@ fn computed_path_should_fail() {
     new_test_ext().execute_with(|| {
         initialize_trust();
         assert_ok!(ZdSeeds::new_seed(Origin::root(), ALICE));
-        assert_noop!(ZdTrust::computed_path(&vec![BOB,CHARLIE]), Error::<Test>::NotSeed);
-        assert_noop!(ZdTrust::computed_path(&vec![ALICE, BOB, 22]), Error::<Test>::WrongPath);
+        assert_noop!(
+            ZdTrust::computed_path(&vec![BOB, CHARLIE]),
+            Error::<Test>::NotSeed
+        );
+        assert_noop!(
+            ZdTrust::computed_path(&vec![ALICE, BOB, 22]),
+            Error::<Test>::WrongPath
+        );
     });
 }
 
@@ -98,15 +130,119 @@ fn get_trust_count_old_should_work() {
 
         // (ALICE, CHARLIE), (ALICE, BOB)
         assert_ok!(ZdTrust::do_trust(&ALICE, &DAVE));
-        assert_eq!(ZdTrust::get_trust_count_old(&ALICE),2);
-        assert_eq!(ZdTrust::get_trust_count(&ALICE),3);
+        assert_eq!(ZdTrust::get_trust_count_old(&ALICE), 2);
+        assert_eq!(ZdTrust::get_trust_count(&ALICE), 3);
 
         assert_ok!(ZdTrust::do_untrust(&ALICE, &DAVE));
-        assert_eq!(ZdTrust::get_trust_count_old(&ALICE),2);
-        assert_eq!(ZdTrust::get_trust_count(&ALICE),2);
+        assert_eq!(ZdTrust::get_trust_count_old(&ALICE), 2);
+        assert_eq!(ZdTrust::get_trust_count(&ALICE), 2);
 
         assert_ok!(ZdTrust::do_untrust(&ALICE, &CHARLIE));
-        assert_eq!(ZdTrust::get_trust_count_old(&ALICE),2);
-        assert_eq!(ZdTrust::get_trust_count(&ALICE),1);
+        assert_eq!(ZdTrust::get_trust_count_old(&ALICE), 2);
+        assert_eq!(ZdTrust::get_trust_count(&ALICE), 1);
+    });
+}
+
+#[test]
+fn is_trust_old_should_work() {
+    new_test_ext().execute_with(|| {
+        ZdReputation::set_step(&TIRStep::SEED);
+
+        assert_eq!(ZdTrust::is_trust_old(&ALICE, &DAVE), false);
+        assert_ok!(ZdTrust::do_trust(&ALICE, &DAVE));
+        assert_eq!(ZdTrust::is_trust_old(&ALICE, &DAVE), false);
+    });
+}
+
+#[test]
+fn get_trust_old_should_work() {
+    new_test_ext().execute_with(|| {
+        initialize_trust();
+
+        // (ALICE, CHARLIE), (ALICE, BOB)
+        assert_eq!(ZdTrust::get_trust_old(&ALICE), vec![BOB, CHARLIE]);
+
+        ZdReputation::set_step(&TIRStep::SEED);
+
+        assert_ok!(ZdTrust::do_trust(&ALICE, &DAVE));
+
+        assert_eq!(ZdTrust::trust_temp_list(&ALICE).trust.0.is_empty(), true);
+        assert_eq!(ZdTrust::trust_temp_list(&ALICE).untrust.0, vec![DAVE]);
+
+        assert_eq!(ZdTrust::get_trust_old(&ALICE), vec![BOB, CHARLIE]);
+
+        assert_ok!(ZdTrust::do_untrust(&ALICE, &CHARLIE));
+        assert_eq!(ZdTrust::get_trust_old(&ALICE), vec![BOB, CHARLIE]);
+    });
+}
+
+#[test]
+fn trust_should_work() {
+    new_test_ext().execute_with(|| {
+        assert_ok!(ZdTrust::trust(Origin::signed(ALICE), BOB));
+        assert_eq!(ZdTrust::is_trust(&ALICE, &BOB), true);
+
+        ZdReputation::set_step(&TIRStep::SEED);
+        assert_ok!(ZdTrust::trust(Origin::signed(ALICE), CHARLIE));
+        assert_eq!(ZdTrust::is_trust(&ALICE, &CHARLIE), true);
+        assert_eq!(ZdTrust::is_trust_old(&ALICE, &CHARLIE), false);
+
+        assert_eq!(ZdTrust::trust_temp_list(&ALICE).trust.0.is_empty(), true);
+        assert_eq!(ZdTrust::trust_temp_list(&ALICE).untrust.0, vec![CHARLIE]);
+
+        let trust_event = Event::zd_trust(crate::Event::Trusted(ALICE, CHARLIE));
+        assert!(System::events()
+            .iter()
+            .any(|record| record.event == trust_event));
+    });
+}
+
+#[test]
+fn untrust_should_work() {
+    new_test_ext().execute_with(|| {
+        initialize_trust();
+        // (ALICE, CHARLIE), (ALICE, BOB)
+        assert_ok!(ZdTrust::untrust(Origin::signed(ALICE), BOB));
+        assert_eq!(ZdTrust::is_trust(&ALICE, &BOB), false);
+
+        ZdReputation::set_step(&TIRStep::SEED);
+        assert_ok!(ZdTrust::untrust(Origin::signed(ALICE), CHARLIE));
+        assert_eq!(ZdTrust::is_trust(&ALICE, &CHARLIE), false);
+        assert_eq!(ZdTrust::is_trust_old(&ALICE, &CHARLIE), true);
+
+        let trust_event = Event::zd_trust(crate::Event::Untrusted(ALICE, BOB));
+        assert!(System::events()
+            .iter()
+            .any(|record| record.event == trust_event));
+    });
+}
+
+#[test]
+fn trust_should_fail() {
+    new_test_ext().execute_with(|| {
+        initialize_trust();
+        assert_noop!(
+            ZdTrust::trust(Origin::signed(ALICE), BOB),
+            Error::<Test>::RepeatTrust
+        );
+        assert_noop!(
+            ZdTrust::trust(Origin::signed(ALICE), ALICE),
+            Error::<Test>::UnableTrustYourself
+        );
+    });
+}
+
+#[test]
+fn untrust_should_fail() {
+    new_test_ext().execute_with(|| {
+        initialize_trust();
+        assert_noop!(
+            ZdTrust::untrust(Origin::signed(ALICE), DAVE),
+            Error::<Test>::NonExistent
+        );
+        assert_noop!(
+            ZdTrust::untrust(Origin::signed(ALICE), ALICE),
+            Error::<Test>::UnableUntrustYourself
+        );
     });
 }
