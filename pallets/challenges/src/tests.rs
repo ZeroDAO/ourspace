5df6e9404617c3ec2e899d436aad1de60bcb4458
DarKing Lee <xiuerdwy@gmail.com>
2021-08-23 17:56:18 +0800

    fix: test challenges

--- pallets/challenges/src/tests.rs
+++ pallets/challenges/src/tests.rs
@@ -175,3 +175,69 @@ new_challenge_should_work! {
         ..NewChallengeData::default()
     },
 }
+
+#[test]
+fn new_challenge_staking_fail() {
+    new_test_ext().execute_with(|| {
+        assert!(
+            ZdChallenges::new(&APP_ID, &DAVE, &PATHINFER, 0, 1000, &TARGET, 100, 0).is_err()
+        );
+        assert_eq!(ZdChallenges::get_metadata(&APP_ID, &TARGET),Metadata::default())
+    });
+}
+
+macro_rules! new_challenge_no_allowed {
+    ($($name:ident: $value:expr,)*) => {
+        $(
+            #[test]
+            fn $name() {
+                new_test_ext().execute_with(|| {
+                    let init_metadata = Metadata {
+                        progress: Progress {
+                            total: $value.0,
+                            done: 0,
+                        },
+                        last_update: 1,
+                        status: $value.1,
+                        ..DEFAULT_METADATA
+                    };
+                    <Metadatas<Test>>::insert(&APP_ID,&TARGET,&init_metadata);
+                    // ChallengeTimeout
+                    System::set_block_number($value.2);
+
+                    assert_noop!(ZdChallenges::new(
+                        &APP_ID,
+                        &CHALLENGER,
+                        &PATHINFER,
+                        10,
+                        10,
+                        &TARGET,
+                        20,
+                        10
+                    ),Error::<Test>::NoChallengeAllowed);
+                });
+            }
+        )*
+    }
+}
+
+new_challenge_no_allowed! {
+    new_challenge_no_allowed_0: (10,Status::FREE,5),
+    new_challenge_no_allowed_1: (10,Status::FREE,2),
+    new_challenge_no_allowed_2: (10,Status::FREE,0),
+    new_challenge_no_allowed_3: (0,Status::FREE,20),
+    new_challenge_no_allowed_4: (0,Status::FREE,1000),
+    new_challenge_no_allowed_5: (0,Status::EVIDENCE,5),
+    new_challenge_no_allowed_6: (0,Status::EXAMINE,5),
+    new_challenge_no_allowed_7: (0,Status::REPLY,5),
+    new_challenge_no_allowed_8: (0,Status::ARBITRATION,5),
+}
+
+/* 
+#[test]
+fn next_should_work() {
+    new_test_ext().execute_with(|| {
+
+    });
+}
+*/
